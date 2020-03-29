c374d896007175f0625851cfcf61e7a96c7e5455
Patrick <urlmacher@gmail.com>
2020-03-29 18:32:00 +0200

    small fixes

--- pages/service.php
+++ pages/service.php
@@ -4,28 +4,14 @@ $site_desc = 'Service, Kontakt';
 include('templates/header.php');
 include('templates/nav-top.php');
 ?>
-<div class="grid">
-	<div class="grid__col grid__col--1-of-2">
-		<div class="agb" id="service">
-			<h3>Kundenservice</h3>
-			<p>Bei Notfällen steht unser Service-Team rund um die Uhr für Sie bereit.</p>
-			<a href="tel:+436644128588">
-				<h3>0664/41 28 588</h3>
-			</a>
-			<p>Für Fragen, Meinungen oder Anregungen kontaktieren Sie uns.</p>
-			<h3>office@rentafritz.com</h3>
-			</br>
-			<h3>Gegen einen geringen Aufpreis bieten wir auch einen Liefer- und Abholservice für unsere Geräte.</h3>
-		</div>
-	</div>
-	<div class="grid__col grid__col--1-of-2">
-		<div class="object product">
-			<a href="./">
-				<div class="home-link">
-					<h3>zurück zur Startseite</h3>
-				</div>
-			</a>
-		</div>
-	</div>
+<div class="secondary-page rentafritz-container">
+
+  <h3 class="secondary-page__title">Kundenservice</h3>
+  <p class="secondary-page__text">Bei Notfällen steht unser Service-Team rund um die Uhr für Sie bereit.</p>
+  <a class="secondary-page__text" href="tel:+436644128588">0664/41 28 588</a>
+  <p class="secondary-page__text">Für Fragen, Meinungen oder Anregungen kontaktieren Sie uns.</p>
+  <a class="secondary-page__text" href="mailto:office@rentafritz.com">office@rentafritz.com</a>
+  <h3 class="secondary-page__title">Gegen einen geringen Aufpreis bieten wir auch einen Liefer- und Abholservice für unsere Geräte.</h3>
+
 </div>
 <?php include('templates/footer.php') ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,17 @@
 <footer class="renta-footer">
-  <div class="renta-footer__link-wrapper">
-    <a class="renta-footer__link" href="/anfahrt#anfahrt">Anfahrt</a>
-    <a class="renta-footer__link" href="/service#service">Service</a>
-    <a class="renta-footer__link" href="/oeffnungszeiten">Öffnungszeiten</a>
-    <a class="renta-footer__link" href="/agb">AGB</a>
-    <a class="renta-footer__link" href="/impressum#impress-link">Impressum</a>
-    <a class="renta-footer__link" href="/datenschutz">Datenschutz</a>
-  </div>
-  <div class="renta-footer__link-wrapper renta-footer__link-wrapper--right">
-    <a class="renta-footer__link" href="https://www.facebook.com/Rentafritz-1731009870517628/"><img class="renta-footer__icon-link" src="../img/facebook-logo.svg" alt="Facebook-Logo"></a>
-    <p class="renta-footer__text">© 2020 Rentafritz</p>
+  <div class="renta-footer__wrapper rentafritz-container">
+    <div class="renta-footer__link-wrapper">
+      <a class="renta-footer__link" href="/anfahrt#anfahrt">Anfahrt</a>
+      <a class="renta-footer__link" href="/service#service">Service</a>
+      <a class="renta-footer__link" href="/oeffnungszeiten">Öffnungszeiten</a>
+      <a class="renta-footer__link" href="/agb">AGB</a>
+      <a class="renta-footer__link" href="/impressum#impress-link">Impressum</a>
+      <a class="renta-footer__link" href="/datenschutz">Datenschutz</a>
+    </div>
+    <div class="renta-footer__link-wrapper renta-footer__link-wrapper--right">
+      <a class="renta-footer__link" href="https://www.facebook.com/Rentafritz-1731009870517628/"><img class="renta-footer__icon-link" src="../img/facebook-logo.svg" alt="Facebook-Logo"></a>
+      <p class="renta-footer__text">© 2020 Rentafritz</p>
+    </div>
   </div>
 </footer>
 
