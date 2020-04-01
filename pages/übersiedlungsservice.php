0e79684626173893220c59ac537a017ac535fc79
Patrick <urlmacher@gmail.com>
2020-04-01 19:06:12 +0200

    siedlpage done

--- pages/übersiedlungsservice.php
+++ pages/übersiedlungsservice.php
@@ -1,57 +1,47 @@
-﻿
-<?php
+﻿<?php
 $title_addition = ' | Siedlservice';
 $site_desc = 'Umzug, Lagerung';
 include('templates/header.php');
 include('templates/nav-top.php');
 ?>
 
-	<div class="flexigrid ">
-		<div class=" box-text">
-			<img class="box-img" src="img/siedlmobil.png">
-		</div>
-		<div class=" box-text">
-			<div id="siedl-tiedl"></div>
-			<p>Unser Ziel ist es Sie mit einem stressfreien und kostengünstigen Umzugserlebnis zu versorgen.</p>
-			<p>Ob Sie privat oder als Unternehmen siedeln, unsere professionellen Mitarbeiter schaffen ihre Gegenstände sicher und effizient wohin Sie wollen, europaweit! </p>
-			<p>Wir bieten auch günstige Lagermöglichkeiten.</p>
-			<h2>ab <span>€49,-</span> pro Stunde</h2>
-			<div class="box-link">
-				<a href="./#reservieren">
-					<h2>Jetzt reservieren</h2>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="line"></div>
-	<div class="flexigrid ">
-		<div class="flexicol">
-			<div class="bimbo-title">
-				<img class="bimbo-img" src="img/shields.png">
-				<h1>Sicherheit</h1>
-				<p>Unsere Mitarbeiter behandeln Ihre Möbel und Güter mit größter Vorsicht. Beim Transport wird alles sorgfältig gepolstert und festgeschnallt.</p>
-			</div>
-		</div>
-		<div class="flexicol">
-			<div class="bimbo-title">
-				<img class="bimbo-img" src="img/hands.png">
-				<h1>Zuverlässigkeit</h1>
-				<p>Wir erscheinen pünktlich zum vereinbarten Termin und behandeln ihre Angelegenheiten mit höchster Diskretion.</p>
-			</div>
-		</div>
-		<div class="flexicol">
-			<div class="bimbo-title">
-				<img class="bimbo-img" src="img/tools.png">
-				<h1>Möbel Ab- und Aufbau</h1>
-				<p>Wir demontieren Ihre Möbel fachgerecht und bauen Sie am Zielort wieder auf. Das dafür benötigte Werkzeug bringen wir mit.</p>
-			</div>
-		</div>
-		<div class="flexicol">
-			<div class="bimbo-title">
-				<img class="bimbo-img" src="img/lock.png">
-				<h1>Lagerung</h1>
-				<p>Möbel können bei uns kostengünstig ein- und zwischengelagert werden.</p>
-			</div>
-		</div>
-	</div>
-	<?php include('templates/footer.php') ?>
+<main class="secondary-page rentafritz-container">
+  <section class="siedl__section-two">
+    <div class="siedl__text-wrapper">
+      <h1 class="siedl__title">Übersiedlungsservice</h1>
+      <p class="siedl__text">Unser Ziel ist es Sie mit einem stressfreien und kostengünstigen Umzugserlebnis zu versorgen.</p>
+      <p class="siedl__text">Ob Sie privat oder als Unternehmen siedeln, unsere professionellen Mitarbeiter schaffen ihre Gegenstände sicher und effizient wohin Sie wollen, europaweit! </p>
+      <p class="siedl__text">Wir bieten auch günstige Lagermöglichkeiten.</p>
+      <h2 class="siedl__price">ab <span>€49,-</span> pro Stunde</h2>
+      <a href="/reservierung/1" class="btn waves-effect waves-light waves-secondary btn__secondary siedl__button">Jetzt Reservieren</a>
+    </div>
+    <img class="siedl__img" src="img/siedlmobil.png">
+  </section>
+
+  <div class="siedl__line"></div>
+
+  <section class="siedl__section-four">
+    <div class="siedl__vignette">
+    <i class="material-icons siedl__vignette-icon">security</i>
+      <h3 class="siedl__title siedl__vignette-title">Sicherheit</h3>
+      <p class="siedl__text siedl__vignette-text">Unsere Mitarbeiter behandeln Ihre Möbel und Güter mit größter Vorsicht. Beim Transport wird alles sorgfältig gepolstert und festgeschnallt.</p>
+    </div>
+    <div class="siedl__vignette">
+    <i class="material-icons siedl__vignette-icon">local_shipping</i>
+      <h3 class="siedl__title siedl__vignette-title">Zuverlässigkeit</h3>
+      <p class="siedl__text siedl__vignette-text">Wir erscheinen pünktlich zum vereinbarten Termin und behandeln ihre Angelegenheiten mit höchster Diskretion.</p>
+    </div>
+    <div class="siedl__vignette">
+    <i class="material-icons siedl__vignette-icon">build</i>
+      <h3 class="siedl__title siedl__vignette-title">Möbel Ab- und Aufbau</h3>
+      <p class="siedl__text siedl__vignette-text">Wir demontieren Ihre Möbel fachgerecht und bauen Sie am Zielort wieder auf. Das dafür benötigte Werkzeug bringen wir mit.</p>
+    </div>
+    <div class="siedl__vignette">
+      <i class="material-icons siedl__vignette-icon">lock</i>
+      <h3 class="siedl__title siedl__vignette-title">Lagerung</h3>
+      <p class="siedl__text siedl__vignette-text">Möbel können bei uns kostengünstig ein- und zwischengelagert werden.</p>
+    </div>
+
+  </section>
+</main>
+<?php include('templates/footer.php') ?>
